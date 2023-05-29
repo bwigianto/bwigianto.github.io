4a67b63953384e656417f50a183beb40a964fb71
Bryan <bwigianto@gmail.com>
2023-05-29 18:41:37 -0500

    Updates

---
+++ precache-manifest.410626129a29fb16305fd13be86a0b87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9e3348c5bdd8d9db735",
+    "url": "/static/css/main.ad3a10b5.chunk.css"
+  },
+  {
+    "revision": "f9e3348c5bdd8d9db735",
+    "url": "/static/js/main.f9e3348c.chunk.js"
+  },
+  {
+    "revision": "3bc8ac50b02e373cc067",
+    "url": "/static/js/1.3bc8ac50.chunk.js"
+  },
+  {
+    "revision": "808a066ce7561e4f228b",
+    "url": "/static/js/2.808a066c.chunk.js"
+  },
+  {
+    "revision": "b117a03f136dce80bebe",
+    "url": "/static/js/runtime~main.b117a03f.js"
+  },
+  {
+    "revision": "67ef979f24ebcc5e1ffeeb4a5f01bbae",
+    "url": "/index.html"
+  }
+];
