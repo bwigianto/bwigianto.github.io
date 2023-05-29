010d71ae526ee5920a7d61551321f838a8b3a6a4
Bryan <bwigianto@gmail.com>
2023-05-29 12:09:33 -0500

    fix undefined

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-function generateRandomMathQuestion() {
+window.onload = function () {
+  function generateRandomMathQuestion() {
     const operators = ['+', '-', '*', '/'];
     const num1 = Math.floor(Math.random() * 10);
     const num2 = Math.floor(Math.random() * 10);
     const operator = operators[Math.floor(Math.random() * operators.length)];
     const questionText = `${num1} ${operator} ${num2}`;
     document.getElementById('question').innerText = questionText;
-}
+    return questionText;
+  }
 
-function checkAnswer() {
+  function checkAnswer() {
     const userAnswer = parseInt(document.getElementById('userAnswer').value);
     const correctAnswer = parseInt(document.getElementById('question').innerText.split(' ')[0]);
     const result = document.getElementById('result');
 
     if (userAnswer === correctAnswer) {
-        result.innerText = 'Correct!';
+      result.innerText = 'Correct!';
     } else {
-        result.innerText = 'Wrong!';
+      result.innerText = 'Wrong!';
     }
-}
+  }
 
-document.getElementById('question').innerText = generateRandomMathQuestion();
+  document.getElementById('question').innerText = generateRandomMathQuestion();
+
+  // Assign the checkAnswer function to the button's onclick event
+  document.querySelector('button').onclick = checkAnswer;
+}
 
